58a7049fc4e0d0d5a5c10c23dcb5ec5515d29aaf
worldwarmworm <st.valeriq@gmail.com>
2022-02-18 18:52:53 +0700

    added controllers

---
+++ src/top-page/dto/find-top-page.dto.ts
@@ -0,0 +1,5 @@
+import { TopLevelCatigory } from '../top-page.model';
+
+export class FindTopPageDto {
+  firstCategory: TopLevelCatigory;
+}
--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,26 +1,27 @@
 export enum TopLevelCatigory {
-	Courses,
-	Services,
-	Book,
-	Products
+  Courses,
+  Services,
+  Book,
+  Products,
 }
 
 export class TopPageModel {
-	firstCategory: TopLevelCatigory;
-	secondCategory: string;
-	title: string;
-	category: string;
-	hh?: {
-		count: number;
-		juniorSalary: number;
-		middleSalary: number;
-		seniorSalary: number;
-	};
-	advantages: {
-		title: string;
-		discription: string;
-	}[];
-	seoText: string;
-	tagsTitle: string;
-	tags: string[];
+  _id: string;
+  firstCategory: TopLevelCatigory;
+  secondCategory: string;
+  title: string;
+  category: string;
+  hh?: {
+    count: number;
+    juniorSalary: number;
+    middleSalary: number;
+    seniorSalary: number;
+  };
+  advantages: {
+    title: string;
+    discription: string;
+  }[];
+  seoText: string;
+  tagsTitle: string;
+  tags: string[];
 }
